923c1aadafd4c7de09ba94d828ad484b60854ab5
Niklas Ekman <niklas@niklasekman.com>
2017-01-19 12:56:31 +0200

    Lade till en if-sats som kollar efter namn
    Också en fin kommentar finns nu i koden som förklarar allt!

--- index.php
+++ index.php
@@ -5,10 +5,22 @@
 	<title>Welcome to Chatbot!</title>
 </head>
 <body>
+
+
+<?php
+/** Kolla om query string parametern (GET parametern) är (==) tom (NULL)
+ *	I fall att den är tom visas ett formulär som ber besökaren fylla i sitt namn
+ */
+if ($_GET['name'] == NULL):
+?>
+
 <form>
 	<label for="name">Name</label>
 	<input type="text" name="name" id="name">
 	<input type="submit" value="Send!">
 </form>
+
+<?php endif ?>
+
 </body>
 </html>
