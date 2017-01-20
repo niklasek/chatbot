b0932d72956a5be7149504d17d64abae7d9d75dd
Niklas Ekman <niklas@niklasekman.com>
2017-01-20 15:20:58 +0200

    Besökaren kan svara boten en gång

--- index.php
+++ index.php
@@ -22,22 +22,31 @@ if ($_GET['name'] == NULL):
 
 <?php
 
-else: 
+elseif ($_GET['reply'] == NULL): 
 
-$name = $_GET['name'];
-$half_name_length = (int) (mb_strlen($name) / 2);
-$remaining_chars = mb_strlen($name) - $half_name_length;
-$name_end = mb_substr($name, $half_name_length, $remaining_chars);
-$name_beginning = mb_substr($name, 0, $half_name_length);
-$botname = $name_end . $name_beginning;
+	$name = $_GET['name'];
+	$half_name_length = (int) (mb_strlen($name) / 2);
+	$remaining_chars = mb_strlen($name) - $half_name_length;
+	$name_end = mb_substr($name, $half_name_length, $remaining_chars);
+	$name_beginning = mb_substr($name, 0, $half_name_length);
+	$botname = $name_end . $name_beginning;
 ?>
 
 <p><strong><?= $botname ?>:</strong> Hej <?= $name ?></p>
 <form>
 	<input type="text" name="reply">
 	<input type="hidden" name="name" value="<?= $name ?>">
+	<input type="hidden" name="botname" value="<?= $botname ?>">
 	<input type="submit" value="reply">
 </form>
+<?php
+else:
+	$name = $_GET['name'];
+	$botname = $_GET['botname'];
+	$reply = $_GET['reply'];
+?>
+<p><strong><?= $botname ?>:</strong> Hej <?= $name ?></p>
+<p><strong><?= $name ?>:</strong> <?= $reply ?></p>
 <?php endif ?>
 
 </body>
